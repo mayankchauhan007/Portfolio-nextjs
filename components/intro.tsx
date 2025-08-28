bb5c057359558752d9c16fbb3441653a21f8549f
mayankchauhan007 <mayankchauhan20837@gmail.com>
2025-08-29 00:50:06 +0530

    added AI agents to resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Mayank Kumar.</span> I'm a{" "}
         <span className="font-bold">Full Stack Developer</span> with{" "}
-        <span className="font-bold">3+ years</span> of experience. I enjoy
+        <span className="font-bold">3.3+ years</span> of experience. I enjoy
         building <span className="italic">scalable web applications</span> using{" "}
         <span className="underline">Next.js, Node.js, React.js and AWS</span>.
       </motion.h1>
@@ -92,7 +92,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="/Mayank_resume.pdf"
+          href="/Mayank_Resume.pdf"
           download
         >
           Download CV{" "}
